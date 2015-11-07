0dcf2bbbb768c361e37805bdd3885fb8899bbbf3
Raivo Laanemets <raivo@infdot.com>
2015-11-07 23:54:23 +0200

    Increase patch version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(simple_template).
-version('1.0.0').
+version('1.0.1').
 title('Logic-free text (HTML) templates').
 author('Raivo Laanemets', 'http://rlaanemets.com/').
 home('https://github.com/rla/simple-template').
