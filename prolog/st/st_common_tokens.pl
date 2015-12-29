ada87cd933db1320139889e0d161c314d9f747be
Raivo Laanemets <raivo@infdot.com>
2015-12-29 16:19:52 +0200

    Refactor more aggressively

--- prolog/st/st_common_tokens.pl
+++ prolog/st/st_common_tokens.pl
@@ -1,8 +1,10 @@
 :- module(st_common_tokens, [
-    collapse/3,
-    text/3,
-    text_codes/3,
-    text_code/3
+    st_collapse//1,
+    st_term//2,
+    st_invalid//1,
+    st_token_term_include/2,
+    st_token_term_dyn_include/2,
+    st_token_term_each/2
 ]).
 
 /** <module> Template tokenizer
@@ -14,14 +16,14 @@ Recognizes tokens from symbol codes.
 
 % Collapses codes into text tokens.
 
-collapse([Token|Tokens]) -->
+st_collapse([Token|Tokens]) -->
     text(Token), !,
-    collapse(Tokens).
+    st_collapse(Tokens).
 
-collapse([Token|Tokens]) -->
-    [Token], collapse(Tokens).
+st_collapse([Token|Tokens]) -->
+    [Token], st_collapse(Tokens).
 
-collapse([]) --> [].
+st_collapse([]) --> [].
 
 text(text(Codes)) -->
     text_codes(Codes).
@@ -35,3 +37,73 @@ text_codes([Code]) -->
 
 text_code(Code) -->
     [Code], { number(Code) }.
+
+% Turns term into an include token.
+
+% FIXME validate path spec.
+
+st_token_term_include(Term, Token):-
+    (   Term =.. [',', File, Var]
+    ->  Token = include(File, Var)
+    ;   Token = include(Term)).
+
+% Turns term into a dynamic include token.
+
+st_token_term_dyn_include(Term, Token):-
+    (   Term = ','(File, Var)
+    ->  Token = dynamic_include(File, Var)
+    ;   Token = dynamic_include(Term)).
+
+% Turns term into an each token.
+
+st_token_term_each(Term, Token):-
+    (   Term = ','(Items, ','(Item, ','(Index, Len)))
+        ->  Token = each(Items, Item, Index, Len)
+        ;   (   Term = ','(Items, ','(Item, Index))
+            ->  Token = each(Items, Item, Index)
+            ;   (   Term = ','(Items, Item)
+                ->  Token = each(Items, Item)
+                ;   throw(error(invalid_each(Term)))))).
+
+% Helper to report invalid instructions.
+% Begin is either {{ or {%.
+
+st_invalid(Begin) -->
+    begin_match(Begin), whites, [C1,C2,C3,C4,C5],
+    {
+        atom_codes(Atom, [C1,C2,C3,C4,C5]),
+        atom_concat(Atom, '...', At),
+        throw(error(invalid_instruction(At)))
+    }.
+
+begin_match("{{") -->
+    "{{".
+
+begin_match("{%") -->
+    "{%".
+
+% Extracts term from input. Term is delimited
+% with the given delimiter (either }} or %}).
+
+st_term(Term, Delimiter) -->
+    codes_delimiter(Delimiter, Codes),
+    {
+        (   read_term_from_codes(Codes, Term, [])
+        ->  (   ground(Term)
+            ->  true
+            ;   throw(error(non_ground_expression(Term))))
+        ;   string_codes(String, Codes),
+            throw(error(invalid_input(String))))
+    }.
+
+% Takes input up to and including the
+% given delimiter (either }} or %}).
+
+codes_delimiter("}}", []) -->
+    "}}", !.
+
+codes_delimiter("%}", []) -->
+    "%}", !.
+
+codes_delimiter(Until, [Code|Codes]) -->
+    [Code], codes_delimiter(Until, Codes).
--- prolog/st/st_simple_tokens.pl
+++ prolog/st/st_simple_tokens.pl
@@ -20,7 +20,7 @@ Recognizes tokens from symbol codes.
 
 st_simple_tokens(Codes, Tokens):-
     phrase(tokens(Tmp1), Codes),
-    phrase(collapse(Tmp2), Tmp1), !,
+    phrase(st_collapse(Tmp2), Tmp1), !,
     Tokens = Tmp2.
 
 tokens(Tokens) -->
@@ -37,10 +37,10 @@ comment -->
     "{{%", string(_), "}}".
 
 token(out(Term)) -->
-    "{{=", whites, term(Term), !.
+    "{{=", whites, st_term(Term, "}}"), !.
 
 token(out_unescaped(Term)) -->
-    "{{-", whites, term(Term), !.
+    "{{-", whites, st_term(Term, "}}"), !.
 
 token(end) -->
     "{{", whites, "end", whites, "}}", !.
@@ -48,65 +48,26 @@ token(end) -->
 token(else) -->
     "{{", whites, "else", whites, "}}", !.
 
-% FIXME validate path spec.
+token(Token) -->
+    "{{", whites, "include ", whites, st_term(Term, "}}"), !,
+    { st_token_term_include(Term, Token) }.
 
 token(Token) -->
-    "{{", whites, "include ", whites, term(Term), !,
-    {
-        (   Term =.. [',', File, Var]
-        ->  Token = include(File, Var)
-        ;   Token = include(Term))
-    }.
-
-token(Token) -->
-    "{{", whites, "dynamic_include ", whites, term(Term), !,
-    {
-        (   Term = ','(File, Var)
-        ->  Token = dynamic_include(File, Var)
-        ;   Token = dynamic_include(Term))
-    }.
+    "{{", whites, "dynamic_include ", whites, st_term(Term, "}}"), !,
+    { st_token_term_dyn_include(Term, Token) }.
 
 token(if(Cond)) -->
-    "{{", whites, "if ", whites, term(Cond), !.
+    "{{", whites, "if ", whites, st_term(Cond, "}}"), !.
 
 token(else_if(Cond)) -->
-    "{{", whites, "else ", whites, "if ", whites, term(Cond), !.
+    "{{", whites, "else ", whites, "if ", whites, st_term(Cond, "}}"), !.
 
 token(Token) -->
-    "{{", whites, "each ", whites, term(Term), !,
-    {
-        (   Term = ','(Items, ','(Item, ','(Index, Len)))
-        ->  Token = each(Items, Item, Index, Len)
-        ;   (   Term = ','(Items, ','(Item, Index))
-            ->  Token = each(Items, Item, Index)
-            ;   (   Term = ','(Items, Item)
-                ->  Token = each(Items, Item)
-                ;   throw(error(invalid_each(Term))))))
-    }.
+    "{{", whites, "each ", whites, st_term(Term, "}}"), !,
+    { st_token_term_each(Term, Token) }.
 
 token(_) -->
-    "{{", whites, [C1,C2,C3,C4,C5],
-    {
-        atom_codes(Atom, [C1,C2,C3,C4,C5]),
-        atom_concat(Atom, '...', At),
-        throw(error(invalid_instruction(At)))
-    }.
+    st_invalid("{{").
 
 token(Code) -->
     [Code].
-
-term(Term) -->
-    term_codes(Codes),
-    {
-        (   read_term_from_codes(Codes, Term, [])
-        ->  (   ground(Term)
-            ->  true
-            ;   throw(error(non_ground_expression(Term))))
-        ;   string_codes(String, Codes),
-            throw(error(invalid_input(String))))
-    }.
-
-term_codes([]) --> "}}", !.
-
-term_codes([Code|Codes]) -->
-    [Code], term_codes(Codes).
