1df24445912fa8685c39271cfb96c4b6d7e498eb
Raivo Laanemets <raivo@infdot.com>
2015-12-29 16:30:27 +0200

    Update version and authors

--- pack.pl
+++ pack.pl
@@ -1,5 +1,6 @@
 name(simple_template).
-version('2.0.0').
+version('1.1.0').
 title('Logic-free text (HTML) templates').
 author('Raivo Laanemets', 'http://rlaanemets.com/').
+author('Matthew Carter', 'http://ahungry.com/').
 home('https://github.com/rla/simple-template').
